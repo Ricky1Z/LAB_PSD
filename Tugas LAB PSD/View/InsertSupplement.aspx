9c75aa00dd41ad1ee7896ba4363809d7419a0760
Endra Djati <115966887+Sash1miboy@users.noreply.github.com>
2024-06-04 16:42:36 +0700

    Add ManageSupplement dan InsertSupplement
    ManageSupplement udah bisa munculin dan udah bisa insert supplement, tapi belom bisa update, edit, dan belom ada validasi

---
+++ Tugas LAB PSD/View/InsertSupplement.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Master/NavBar.Master" AutoEventWireup="true" CodeBehind="InsertSupplement.aspx.cs" Inherits="Tugas_LAB_PSD.View.InsertSupplement" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <h1>Insert New Supplement</h1>
+    <div>
+        <asp:Label ID="SupplementNamelbl" runat="server" Text="Supplement Name: "></asp:Label>
+        <asp:TextBox ID="SuplementNameTxt" runat="server"></asp:TextBox>
+    </div>
+    <div>
+        <asp:Label ID="SupplementExDatelbl" runat="server" Text="Supplement Expire Date [Format: mm/dd/yyyy]: "></asp:Label>
+        <asp:TextBox ID="SupplementExDateTxt" runat="server"></asp:TextBox>
+    </div>
+    <div>
+        <asp:Label ID="SupplementPricelbl" runat="server" Text="Supplement Price: "></asp:Label>
+        <asp:TextBox ID="SupplementPriceTxt" runat="server"></asp:TextBox>
+    </div>
+    <div>
+        <asp:Label ID="SupplementTypelbl" runat="server" Text="Supplement Type: "></asp:Label>
+        <asp:DropDownList ID="SupplementTypeDropDown" runat="server"></asp:DropDownList>
+    </div>
+    <asp:Button ID="InsertBtn" runat="server" Text="Insert" OnClick="InsertBtn_Click" />
+    
+
+</asp:Content>
---
+++ Tugas LAB PSD/View/InsertSupplement.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Tugas_LAB_PSD.Model;
+using Tugas_LAB_PSD.Repository;
+
+namespace Tugas_LAB_PSD.View
+{
+    public partial class InsertSupplement : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SupplementTypeRepository STRepo = new SupplementTypeRepository();
+            if (IsPostBack == false)
+            {
+                List<String> supplementTypeName = STRepo.GetSupplementTypeName(); 
+                SupplementTypeDropDown.DataSource = supplementTypeName;
+                SupplementTypeDropDown.DataBind();
+            }
+        }
+
+
+        protected void InsertBtn_Click(object sender, EventArgs e)
+        {
+            SupplementRepository supplementRepository = new SupplementRepository();
+            SupplementTypeRepository STRepo = new SupplementTypeRepository();
+            String supplementName = SuplementNameTxt.Text;
+            DateTime supplementExDate = DateTime.Parse(SupplementExDateTxt.Text);
+            int supplementPrice = Convert.ToInt32(SupplementPriceTxt.Text);
+            String supplementDropDown = SupplementTypeDropDown.Text;
+            int supplementTypeID = STRepo.GetSupplementTypeIDByName(supplementDropDown);
+            supplementRepository.InsertSupplement(supplementName, supplementExDate, supplementPrice, supplementTypeID);
+
+            Response.Redirect("~/View/ManageSupplement.aspx");
+        }
+    }
+}
